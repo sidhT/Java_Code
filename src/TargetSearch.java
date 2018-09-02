302c53aed6d3ed4d290d68b4be05177e330a3c90
Kiran Vadakkath <kiranvadakath@gmail.com>
2018-09-02 12:42:57 -0400

    Leet Code - Two Sum

---
+++ src/TargetSearch.java
@@ -0,0 +1,43 @@
+/*
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+pssume that each input would have exactly one solution, and you may not use the same element twice.
+*/
+
+import java.util.Arrays;
+class TargetSearch {
+    public static void main(String[] args){
+        //System.out.println("This is a test");
+        int [] arr = {8,7,5,2,1,4,2,1};
+        int target = 3;
+        int i;
+
+        Arrays.sort(arr);
+
+        disp(arr);
+
+        int l = arr.length;
+
+        int [] bitmap = new int[arr[l-1]+1];
+
+        for (i=0;i<l;i++)
+            bitmap[arr[i]] = 1;
+
+
+        for (i=0; i<l;i++)
+            try {
+                if (bitmap[target - arr[i]] == 1)
+                    System.out.println("Matching numbers are " + arr[i] + " " + (target - arr[i]));
+                    break;
+            }
+            catch (Exception e){
+
+            }
+
+    }
+
+    public static void disp(int [] arr){
+        for (int i=0; i<arr.length;i++)
+            System.out.print(arr[i]+", ");
+    }
+}
